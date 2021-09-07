a3d08b4a2ab1b775cff1f17fcab71ae46a32e065
Tanvi <tanvigera3004@gmail.com>
2021-09-08 00:22:09 +0530

    Checkpoint 5 (final)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,10 +15,29 @@ Flight SpiceJet=new Flight("A456","Spice Jet",500,78);
         // Indigo.flightNumber="A456"; (Error while accessing private variables)
         Indigo.setAirline("British Airways");
         Indigo.getAirline();
+        //checkpoint 5 Test Case
+        RegularTicket ticket1=new RegularTicket("ArNa123","London","America"
+                ,Indigo,"3/04/2021","5/04/2021","13:25","17:45",
+                "56",9088,false,"water");
+
+        String[] loc = {"LA","San-francisco","Washington"};
+        //checkpoint 5 Test Case
+        TouristTicket ticket2=new TouristTicket("Tamhg","London","America"
+                ,SpiceJet,"3/04/2021","5/04/2021","13:25","17:45",
+                "56",9088,false,"New jersey",loc);
+
+        printTicketDetails(ticket1);
+        printTicketDetails(ticket2);
+
 
 
     }
+    public static void printTicketDetails(Ticket ticket )
+    {
 
+        System.out.println(ticket.getPnrNumber());
+
+    }
 
 
 }

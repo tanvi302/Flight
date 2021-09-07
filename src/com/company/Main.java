19dbd20e79dcf67fea5cab9171acff1d2345fb0c
Tanvi <tanvigera3004@gmail.com>
2021-09-08 00:19:10 +0530

    Checkpoint 2

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //checkpoint 2 Test case
+Passenger person=new Passenger ("Rohini","New Delhi","Delhi",
+        "Tanvi","987343241","xyz@gmail.com");
+
+Flight SpiceJet=new Flight("A456","Spice Jet",500,78);
+
+
+    }
+    
+
+
+}
